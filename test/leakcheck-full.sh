574b757b5c36fc544064a07f8c9ef2e1a7204c13
Jeff Johnson <n3npq@mac.com>
2017-01-25 04:21:08 -0500

    - fix: parser.c memory leak.

--- test/leakcheck-full.sh
+++ test/leakcheck-full.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-../build.sh
 cd `dirname $0`/..
+./scripts/build.sh
+
+valgrind="valgrind -v --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes"
+
 mkdir -p build/tmp/leakcheck-full
-valgrind --leak-check=full --show-leak-kinds=all \
+$valgrind \
     bin/flatcc_d -a -o build/tmp/leakcheck-full --prefix zzz --common-prefix \
     hello test/monster_test/monster_test.fbs
